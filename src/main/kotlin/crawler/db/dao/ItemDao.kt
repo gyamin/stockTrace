e4b1c403c6778dfc4e6ad9ccb7b502b8d32f1e9e
gyamin <5550621+gyamin@users.noreply.github.com>
2022-01-24 23:59:33 +0900

    RowMapper使って、任意のSELECT文をオブジェクトにマッピング

--- src/main/kotlin/crawler/db/dao/ItemDao.kt
+++ src/main/kotlin/crawler/db/dao/ItemDao.kt
@@ -1,11 +1,15 @@
 package crawler.db.dao
 
 import crawler.db.model.ItemBean
+import org.jdbi.v3.core.mapper.RowMapper
+import org.jdbi.v3.core.statement.StatementContext
 import org.jdbi.v3.sqlobject.customizer.Bind
 import org.jdbi.v3.sqlobject.customizer.BindBean
 import org.jdbi.v3.sqlobject.kotlin.RegisterKotlinMapper
 import org.jdbi.v3.sqlobject.statement.SqlQuery
 import org.jdbi.v3.sqlobject.statement.SqlUpdate
+import java.sql.ResultSet
+import java.sql.SQLException
 
 interface ItemDao {
     @SqlQuery("""SELECT code, trading_name, market_type, sector33_code, sector33_name, sector17_code, sector17_name, 
--- src/test/kotlin/crawler/CrawlerRunnerTest.kt
+++ src/test/kotlin/crawler/CrawlerRunnerTest.kt
@@ -1,29 +1,47 @@
 package crawler
 
 import crawler.db.DbConnection
-import crawler.db.dao.ItemDao
 import crawler.db.model.ItemBean
 import kotlinx.coroutines.runBlocking
-import org.jdbi.v3.core.kotlin.mapTo
-import org.jdbi.v3.sqlobject.kotlin.onDemand
+import org.jdbi.v3.core.mapper.RowMapper
+import org.jdbi.v3.core.statement.StatementContext
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import java.sql.ResultSet
+import java.sql.SQLException
 
 class CrawlerRunnerTest {
 
     @Test
-    internal fun testRunCrawling() = runBlocking {
+    internal fun testRunCrawling(): Unit = runBlocking {
+        // Daoを使わずに、RowMapperを利用してデータ取得
         val dbConnection = DbConnection()
         val jdbi = dbConnection.getConnection()
-        val items = jdbi.useHandle<Exception> { handle ->
-            val qry = """
-                SELECT code, trading_name, market_type, sector33_code, sector33_name, 
+        val handle = jdbi.open()
+        val qry = """
+            SELECT code, trading_name, market_type, sector33_code, sector33_name, 
                     sector17_code, sector17_name, scale_code, scale_name, created_at, updated_at FROM items limit 10
-            """.trimIndent()
-            handle.createQuery(qry).mapTo<ItemBean>().list()
-        }
-
-        val crawlerRunner = CrawlerRunner(null)
+        """.trimIndent()
+        val items = handle.createQuery(qry).map(ItemBeanMapper()).list()
+        // クローリング処理 10件で実行してみたら
+        val crawlerRunner = CrawlerRunner(items as List<ItemBean>?)
         crawlerRunner.runCrawling()
+    }
+}
+
+internal class ItemBeanMapper : RowMapper<ItemBean?> {
+    @Throws(SQLException::class)
+    override fun map(rs: ResultSet, ctx: StatementContext?): ItemBean {
+        return ItemBean(rs.getString("code"),
+            rs.getString("trading_name"),
+            rs.getString("market_type"),
+            rs.getInt("sector33_code"),
+            rs.getString("sector33_name"),
+            rs.getInt("sector17_code"),
+            rs.getString("sector17_name"),
+            rs.getInt("scale_code"),
+            rs.getString("scale_name"),
+            rs.getString("created_at"),
+            rs.getString("updated_at")
+        )
     }
 }
