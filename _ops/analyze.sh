a585ef79c647b860a41ab8b0ba92b738e13112a0
nogami <5550621+gyamin@users.noreply.github.com>
2022-07-19 00:41:57 +0900

    #5 解析シェル修正

--- _ops/analyze.sh
+++ _ops/analyze.sh
@@ -5,30 +5,26 @@ shellName=$(basename $0)
 execDir=$(cd $(dirname $0); pwd)
 rootDir=$(cd ${execDir}; cd "./.."; pwd)
 
-# DB接続設定
-dbHost="localhost"
-dbUser="stocktrace"
-dbPort="15432"
-dbName="loc_stocktrace"
-
 # ---------------------------
 # Main
 # ---------------------------
 
 ## パラメータチェック
 ### 名前付きパラメータ取得 -s=sql, -t=type
-while getopts s:t: OPT
+while getopts s:t:e: OPT
 do
   case $OPT in
     s) sql=$OPTARG
        ;;
-    p) type=$OPTARG
+    t) type=$OPTARG
+       ;;
+    e) env=$OPTARG
        ;;
   esac
 done
 
 ### 実行sqlファイル確認
-if [ -z "${sql}" ];then
+if [ -z "${sql}" ]; then
   echo "Usage: ${fileName} [-s sql_file_name]" 1>&2
   exit 1
 fi
@@ -36,11 +32,19 @@ fi
 sqlFilePath=$(find ${execDir}/sql -name "${sql}.sql")
 
 if [ -z "${sqlFilePath}" ];then
-  echo "${sql} does not exits" 1>&2
+  echo "${sql}.sql file does not exits" 1>&2
   exit 1
 fi
 
+### env確認
+if [[ ! "${env}" =~ dev|stg|prd ]]; then
+  echo "-e must be dev|stg|prd" 1>&2
+  exit 1
+fi
+
+source .pgenv.${env}
+
 ## 処理実行
 dateTime=$(date "+%Y%m%d_%H%M%S")
-psql -h ${dbHost} -U ${dbUser} -d ${dbName} -f ${sqlFilePath} -A -F, > "${dateTime}_${sql}.csv"
+psql -h ${DB_HOST} -U ${DB_USER} -d ${DB_NAME} -f ${sqlFilePath} -A -F,
 # ---------------------------
